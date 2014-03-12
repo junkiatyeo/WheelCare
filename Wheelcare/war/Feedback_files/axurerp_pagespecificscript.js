cfcbc92fb7f453e1358a8096f0faad6dd92169c6
Yeo Jun Kiat <jkyeo89@gmail.com>
2014-03-13 01:07:13 +0800

    Wheelcare Commit #04
    Revised Feedback functionalities

--- Wheelcare/war/Feedback_files/axurerp_pagespecificscript.js
+++ Wheelcare/war/Feedback_files/axurerp_pagespecificscript.js
@@ -1,10 +1,10 @@
-﻿for(var i = 0; i < 7; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+﻿for(var i = 0; i < 24; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
 
 $axure.eventManager.pageLoad(
 function (e) {
 
 });
-
+gv_vAlignTable['u3'] = 'center';gv_vAlignTable['u21'] = 'top';gv_vAlignTable['u13'] = 'center';
 u4.style.cursor = 'pointer';
 $axure.eventManager.click('u4', function(e) {
 
@@ -14,7 +14,7 @@ if (true) {
 
 }
 });
-
+gv_vAlignTable['u19'] = 'top';gv_vAlignTable['u17'] = 'top';gv_vAlignTable['u22'] = 'top';
 u5.style.cursor = 'pointer';
 $axure.eventManager.click('u5', function(e) {
 
@@ -24,4 +24,7 @@ if (true) {
 
 }
 });
-gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u3'] = 'center';
+gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u9'] = 'center';$('#u14').attr('axSubmit', 'u23');
+gv_vAlignTable['u20'] = 'top';gv_vAlignTable['u15'] = 'top';$('#u16').attr('axSubmit', 'u23');
+gv_vAlignTable['u11'] = 'center';$('#u18').attr('axSubmit', 'u23');
+gv_vAlignTable['u7'] = 'top';
---
+++ Wheelcare/war/ThankyouFeedback_files/axurerp_pagespecificscript.js
@@ -0,0 +1,27 @@
+﻿for(var i = 0; i < 7; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+
+u4.style.cursor = 'pointer';
+$axure.eventManager.click('u4', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('Home.html');
+
+}
+});
+
+u5.style.cursor = 'pointer';
+$axure.eventManager.click('u5', function(e) {
+
+if (true) {
+
+    self.location.href="resources/reload.html#" + encodeURI($axure.globalVariableProvider.getLinkUrl($axure.pageData.url));
+
+}
+});
+gv_vAlignTable['u6'] = 'top';gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u3'] = 'center';
